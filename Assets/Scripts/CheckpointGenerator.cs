54558699c96fd2fb308566832dc6074c1cb0433a
mutelnwza <mutemallow@gmail.com>
2025-07-10 19:33:29 +0700

    Checkpoint ore needed generator

--- Assets/Scripts/CheckpointGenerator.cs
+++ Assets/Scripts/CheckpointGenerator.cs
@@ -1,13 +1,14 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CheckpointGenerator : MonoBehaviour
 {
     [SerializeField] GameObject checkpointGO;
-    int[] oresCount = new int[4];
+    Checkpoint[] checkpoints;
+    int[] oresCount = new int[4]; //numbers of ore in the level (copper/iron/gold/emerald)
 
     public void SpawnCheckpointObject(Vector2Int[] points, int playerPosIndex, int[] ores)
     {
-        oresCount = ores;
         for (int i = 0; i < points.Length; i++)
         {
             var go = Instantiate(checkpointGO, new Vector3(points[i].x, points[i].y, 0), Quaternion.identity);
@@ -16,11 +17,39 @@ public class CheckpointGenerator : MonoBehaviour
                 go.GetComponent<Checkpoint>().isSpawnpoint = true;
                 go.GetComponent<Checkpoint>().cleared = true;
             }
+            else { checkpoints[i] = go.GetComponent<Checkpoint>(); }
         }
+        SetOresNeedOnEachCheckpoint();
+    }
+
+    List<int[]> SplitOres(int[] oreCounts)
+    {
+        List<int[]> result = new List<int[]>();
+
+        foreach (int count in oreCounts)
+        {
+            int[] parts = new int[4]; // default is [0,0,0,0]
+            int remaining = count;
+
+            for (int i = 0; i < 3; i++)
+            {
+                parts[i] = Random.Range(0, remaining + 1);
+                remaining -= parts[i];
+            }
+
+            parts[3] = remaining;
+            result.Add(parts);
+        }
+
+        return result;
     }
 
     void SetOresNeedOnEachCheckpoint()
     {
-        
+        List<int[]> oresSets = SplitOres(oresCount);
+        for (int i = 0; i < 4; i++)
+        {
+            checkpoints[i].Setup(oresSets[i][0], oresSets[i][1], oresSets[i][2], oresSets[i][3]);
+        }
     }
 }
