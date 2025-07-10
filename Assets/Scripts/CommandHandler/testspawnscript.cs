6b4b18433a78b35e98a72bae0aa1010ce1a4fb0d
SATU-YANANISSORN <satu20102006@gmail.com>
2025-07-10 19:54:50 +0700

    stat inventory
    -use InventoryStatTracker.instance.GetItemCount(ItemName); to access data ore

--- Assets/Scripts/CommandHandler/testspawnscript.cs
+++ Assets/Scripts/CommandHandler/testspawnscript.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 public class testspawnscript : MonoBehaviour
 {
     public InventoryManager inventoryManager;
-
     public DropItem[] itemsToPickup;
 
     public void PickUpItem(int id)
     {
 
         bool result = inventoryManager.AddItem(itemsToPickup[id]);
+   
+
+
         if (result == true)
         {
             Debug.Log("Item added");
