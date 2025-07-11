add18f1a6ab91c1d8d70793cfefb61cb7abc1f5f
mutelnwza <mutemallow@gmail.com>
2025-07-11 10:18:42 +0700

    camera bounding

--- Assets/Scripts/Managers/CameraFollowPlayer.cs
+++ Assets/Scripts/Managers/CameraFollowPlayer.cs
@@ -23,23 +23,23 @@ public class CameraSetUp : MonoBehaviour
 
     public void SetBounds()
     {
-        if (tilemap == null || confinerCollider == null) return;
+        // if (tilemap == null || confinerCollider == null) return;
 
-        Bounds bounds = tilemap.localBounds;
+        // Bounds bounds = tilemap.localBounds;
 
-        Vector2 min = bounds.min;
-        Vector2 max = bounds.max;
+        // Vector2 min = bounds.min;
+        // Vector2 max = bounds.max;
 
-        Vector2[] points = new Vector2[4];
+        // Vector2[] points = new Vector2[4];
 
-        points[0] = new Vector2(min.x, min.y);
-        points[1] = new Vector2(min.x, max.y);
-        points[2] = new Vector2(max.x, max.y);
-        points[3] = new Vector2(max.x, min.y);
+        // points[0] = new Vector2(min.x, min.y);
+        // points[1] = new Vector2(min.x, max.y);
+        // points[2] = new Vector2(max.x, max.y);
+        // points[3] = new Vector2(max.x, min.y);
 
-        confinerCollider.pathCount = 1;
-        confinerCollider.SetPath(0, points);
-        cinemachineConfiner2D.InvalidateBoundingShapeCache();
+        // confinerCollider.pathCount = 1;
+        // confinerCollider.SetPath(0, points);
+        // cinemachineConfiner2D.InvalidateBoundingShapeCache();
     }
 
 }
